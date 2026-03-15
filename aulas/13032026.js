51786d05942daac8a66f38db8987609612acb049
Professor Nilson <nilsongithub@gmail.com>
2026-03-15 19:09:07 -0300

    tarefa 15/03/2026

--- aulas/13032026.js
+++ aulas/13032026.js
@@ -10,3 +10,32 @@ function resultadoDivisaoSocio(lucro,lucro2){
     return soma(lucro,lucro2)/2
 }
 
+
+// Agora crie uma função que recebe um argumento do
+//  tipo aritmético(+,-,/,*) e faça a função desejada desse argumento
+//  com lucro e lucro 2
+
+
+function calculadora(ari,a,b){
+    switch(ari){
+         case '+':
+            resultado = a + b;
+            break;
+        case '-':
+            resultado = a - b;
+            break;
+        case '*':
+            resultado = a * b;
+            break;
+        case '/':
+            resultado =  a / b;
+            break;
+        default:
+            resultado = 'Operador inválido';        
+    
+    }
+    return resultado
+
+}
+
+console.log(calculadora("/",lucro,lucro2))
