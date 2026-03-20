29be6ceb8e2d36826980c6d6e45c0a5d435b14df
Nilson-DataScience <nilsongithub@gmail.com>
2026-03-20 07:53:32 -0300

    update

--- aulas/19032026.js
+++ aulas/19032026.js
@@ -5,5 +5,26 @@
 //Números
 let numero = 30
 let decimal = 20.30
+
 // String
-let nome = "Não sei, você não sabe?"
+let nome = "não sei, você não sabe?"
+let nome1 = "NÃO SEI, VOCÊ NÃO SABE?"
+
+console.log(nome === nome1); // false
+
+const inputMinusculo = nome1.toLowerCase();
+
+console.log(nome === inputMinusculo); // true
+
+const senha = "minhaSenha123"
+console.log(senha.length) // 13 caracteres
+
+
+// Booleanos
+
+let nv = true;
+let nz = false;
+
+if(nv!==nz){
+    console.log(`nv é diferente, pois ele é ${nv}`)
+}
