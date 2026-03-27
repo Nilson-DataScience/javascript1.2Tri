9a8c71bf48491f5cc84258e944c063a056848d7b
Nilson-DataScience <nilsongithub@gmail.com>
2026-03-27 07:56:23 -0300

    update

---
+++ aulas/27032026.js
@@ -0,0 +1,18 @@
+// Criar Array 
+
+const filaNumber = [1,2,3,4,5,6];
+
+const filaStringAmigos = ["Ana Julia","Bob","Patrick"]
+
+function lerAmigos(filaStringAmigos){
+    console.log(`Você tem ${filaStringAmigos.length} amigos`)
+    
+    for(let i = 0; i < filaStringAmigos.length;i++){
+        console.log(filaStringAmigos[i])
+    if(filaStringAmigos[i] === "Bob"){
+            console.log(`Index do seu melhor amigo é ${i}`)
+
+        }
+    }
+}
+
